79b8c341df9761bd021f1c7ce2439851a036ca8e
joseAf28 <josefilipeafonso@hotmail.com>
2023-04-01 16:37:56 +0100

    mpi error exchange particles fixed; the vscode messages of merge conflict were erased

--- diagnostics/plotter.py
+++ diagnostics/plotter.py
@@ -44,11 +44,7 @@ def readField_rank(rank, charge_field, field_type):
 results_path = "/home/jose/Desktop/FCPIC/results/"
 
 nb_ranks = 4
-<<<<<<< HEAD
 counter = 700
-=======
-counter = 50
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
 
 
 ##number and orientation of each process
@@ -101,11 +97,8 @@ for i in range(0, nb_ranks):
 snapshots_charge = []
 snapshots_Ex = []
 snapshots_Ey = []
-<<<<<<< HEAD
 
 image_counter = 0
-=======
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
 for count_plot in range(0, counter):
     big_charge_dummy = []
     Ex_dummy = []
@@ -122,12 +115,8 @@ for count_plot in range(0, counter):
         Ey_dummy.append(Ey_aux)
 
 
-<<<<<<< HEAD
     # for i in range(len(charge_field[0][count_plot])-1, 0, -1):
     for i in range(0, len(charge_field[0][count_plot])):
-=======
-    for i in range(len(charge_field[0][count_plot])-1, 0, -1):
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
         charge_aux = charge_field[0][count_plot][i] + charge_field[1][count_plot][i]
         big_charge_dummy.append(charge_aux)
 
@@ -148,7 +137,6 @@ for count_plot in range(0, counter):
     snapshots_Ex.append(Ex_dummy)
     snapshots_Ey.append(Ey_dummy)
 
-<<<<<<< HEAD
 
     # plt.figure(image_counter)
     # plt.imshow(big_charge_dummy, interpolation ='nearest')
@@ -184,36 +172,15 @@ for count_plot in range(0, counter):
 fps = 30
 nSeconds = math.floor(counter/fps)
 print(nSeconds)
-=======
-    # plt.imshow(big_charge,  cmap="Greens", interpolation ='nearest')
-    # plt.xlabel(r"$x$")
-    # plt.ylabel(r"$y$")
-    # plt.title(r"$\rho$")
-    
-    # plt.savefig(results_path + "/plots/test_"+ str(count_plot) +"_2.png")
-
-fps = 10
-nSeconds = math.floor(counter/fps)
-print(nSeconds)
-# snapshots = [ np.random.rand(5,5) for _ in range( nSeconds * fps ) ]
-
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure( figsize=(8,8) )
 
 a = snapshots_charge[0]
-<<<<<<< HEAD
 im = plt.imshow(a, interpolation='spline16')
 plt.xlabel(r"$x$")
 plt.ylabel(r"$y$")
 plt.title(r"$\rho$")
 plt.colorbar()
-=======
-im = plt.imshow(a, interpolation='nearest', cmap="Greens")
-plt.xlabel(r"$x$")
-plt.ylabel(r"$y$")
-plt.title(r"$\rho$")
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
 
 def animate_func(i):
     if i % fps == 0:
@@ -227,54 +194,6 @@ anim = animation.FuncAnimation(fig, animate_func,
                                interval = 1000 / fps, # in ms
                                )
 
-<<<<<<< HEAD
 anim.save('charge_2species_1ppc_700_mod_vert_square_anim.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
 
 print('Charge Anim Done!')
-=======
-anim.save('test_charge_anim.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
-
-print('Charge Anim Done!')
-
-
-
-
-
-
-
-
-
-##Good example I found online
-
-# import numpy as np
-# def func(x, y):
-#     return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
-
-# rng = np.random.default_rng()
-# points = rng.random((1000, 2))
-# values = func(points[:,0], points[:,1])
-
-# grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
-
-# from scipy.interpolate import griddata
-# grid_z0 = griddata(points, values, (grid_x, grid_y), method='nearest')
-# grid_z1 = griddata(points, values, (grid_x, grid_y), method='linear')
-# grid_z2 = griddata(points, values, (grid_x, grid_y), method='cubic')
-
-# import matplotlib.pyplot as plt
-# plt.subplot(221)
-# plt.imshow(func(grid_x, grid_y).T, extent=(0,1,0,1), origin='lower')
-# plt.plot(points[:,0], points[:,1], 'k.', ms=1)
-# plt.title('Original')
-# plt.subplot(222)
-# plt.imshow(grid_z0.T, extent=(0,1,0,1), origin='lower')
-# plt.title('Nearest')
-# plt.subplot(223)
-# plt.imshow(grid_z1.T, extent=(0,1,0,1), origin='lower')
-# plt.title('Linear')
-# plt.subplot(224)
-# plt.imshow(grid_z2.T, extent=(0,1,0,1), origin='lower')
-# plt.title('Cubic')
-# plt.gcf().set_size_inches(6, 6)
-# plt.show()
->>>>>>> a400d65807475516a0dc3c4337f9839133295f59
